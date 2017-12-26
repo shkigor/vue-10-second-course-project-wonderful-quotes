f4d1c95f199b4fed21af3907e2bf1141623ebc83
Igor Shklyar <igor.shklyar@gmail.com>
2017-12-26 17:33:13 +0200

    137 Adding Quotes with Custom Events

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,31 @@
 <template>
   <div class="container">
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
     <app-quote-grid :quotes="quotes"></app-quote-grid>
   </div>
 </template>
 
 <script>
   import QuoteGrid from './components/QuoteGrid';
+  import NewQuote from './components/NewQuote';
 
   export default {
     data: function () {
       return {
         quotes: [
-                'Just a Quote to see something',
-                'Just another Quote to see something else',
+                'Just a Quote to see something'
         ],
         maxQuotes: 10
       }
     },
+    methods: {
+      newQuote(quote) {
+        this.quotes.push(quote);
+      }
+    },
     components: {
-      appQuoteGrid: QuoteGrid
+      appQuoteGrid: QuoteGrid,
+      appNewQuote: NewQuote
     }
 
   }

99367a2a7e22083d2dbcbc949644f9bafc7a44ce
Ihor Shkliar <igor.shklyar@gmail.com>
2020-07-15 18:43:28 +0300

    Create new vue CLI 3 default project.
    1. Install vue cli 3:
    npm install -g @vue/cli
    
    2: Create new project:
    vue create vue-10-second-course-project-wonderful-quotes

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,28 @@
 <template>
-  <div class="container">
-    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
-    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
-    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
-    <div class="row">
-      <div class="col-sm-12 text-center">
-        <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
-      </div>
-    </div>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-  import Header from './components/Header';
-  import QuoteGrid from './components/QuoteGrid';
-  import NewQuote from './components/NewQuote';
+import HelloWorld from './components/HelloWorld.vue'
 
-  export default {
-    data: function () {
-      return {
-        quotes: [
-                'Just a Quote to see something'
-        ],
-        maxQuotes: 10
-      }
-    },
-    methods: {
-      newQuote(quote) {
-        if (this.quotes.length >= this.maxQuotes) {
-          return alert('Please delete Quotes first!');
-        }
-        this.quotes.push(quote);
-      },
-      deleteQuote(index) {
-        // let index = this.quotes.indexOf(quote);
-        if (index > -1) {
-          this.quotes.splice(index, 1);
-        }
-      }
-    },
-    watch: {
-      quotes: function (value) {
-        if (value.length >= this.maxQuotes) {
-          console.log('Quotes full!');
-        }
-      }
-    },
-    components: {
-      appHeader: Header,
-      appQuoteGrid: QuoteGrid,
-      appNewQuote: NewQuote
-    }
-
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
   }
+}
 </script>
 
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
